a75c3c103d320965731c183aef2c1d4b24afaec0
carolinafrederico <iam@carolinafrederico.com>
2025-04-11 13:42:49 -0400

    List

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const PORT = 3003;
 app.get('/', (req, res) => {
     res.send('Express Server Home')
 })
-
 app.get('/home', (req, res) => {
     res.send('<h1>Hello World</h1>')
-
+})
+app.get('/list', (req, res) => {
+    res.send(`
+        <h1>My List</h1>
+        <ul>
+        <li>Topic 1</li>
+        <li>Topic 2</li>
+        <li>Topic 3</li>
+        </ul>
+        `)
 })
 app.listen(PORT, () => {
     console.log(`Express Server is listening on PORT:${PORT}`)
