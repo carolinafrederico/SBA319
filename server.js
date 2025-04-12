accfa96476d1f4a7d89776c652d6a9d793a49d05
carolinafrederico <iam@carolinafrederico.com>
2025-04-12 17:15:29 -0400

    /json

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const PORT = 3003;
 
 app.get('/', (req, res) => {
     res.send('Express Server Home')
-})
+});
 app.get('/home', (req, res) => {
     res.send('<h1>Hello World</h1>')
-})
+});
 app.get('/list', (req, res) => {
     res.send(`
         <h1>My List</h1>
@@ -17,7 +17,18 @@ app.get('/list', (req, res) => {
         <li>Topic 3</li>
         </ul>
         `)
+});
+app.get('/go-home', (req, res) => {
+    res.redirect('/home');
 })
+app.get ('/json', (req, res)=>{
+    res.json({
+        author: "Carolinda",
+        ratings: [100, 95, 90],
+        topic: "Hard News",
+        published: true
+    }) 
+})    
 app.listen(PORT, () => {
     console.log(`Express Server is listening on PORT:${PORT}`)
-}) 
+})
