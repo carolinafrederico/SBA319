0c1c2b7423d0053a390b603a87130d9a9e393636
carolinafrederico <iam@carolinafrederico.com>
2025-04-13 01:11:56 -0400

    Register

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express()
 const PORT = 3003;
+const bcrypt = require('bcrypt')
 
+const users =[]
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res)=>{
     res.render('index.ejs', {name: 'Carolinda'})
 
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs',)
+})
+app.post('/login', (req, res))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs',)
+})
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+
+        })
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/register')
+    }
+
+})
 
 app.listen(PORT, () => {
     console.log(`Express Server is listening on PORT:${PORT}`)
