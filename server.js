c73164c8535f3eb44c9908df917804cd44dcbdd9
carolinafrederico <iam@carolinafrederico.com>
2025-04-12 23:46:18 -0400

    Login and Register pages

--- server.js
+++ server.js
@@ -2,33 +2,14 @@ const express = require('express');
 const app = express()
 const PORT = 3003;
 
-app.get('/', (req, res) => {
-    res.send('Express Server Home')
-});
-app.get('/home', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-});
-app.get('/list', (req, res) => {
-    res.send(`
-        <h1>My List</h1>
-        <ul>
-        <li>Topic 1</li>
-        <li>Topic 2</li>
-        <li>Topic 3</li>
-        </ul>
-        `)
-});
-app.get('/go-home', (req, res) => {
-    res.redirect('/home');
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs', {name: 'Carolinda'})
+
 })
-app.get ('/json', (req, res)=>{
-    res.json({
-        author: "Carolinda",
-        ratings: [100, 95, 90],
-        topic: "Hard News",
-        published: true
-    }) 
-})    
+
+
 app.listen(PORT, () => {
     console.log(`Express Server is listening on PORT:${PORT}`)
-})
+});
