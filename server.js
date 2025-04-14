ac632c537c3a6d5283b35545a2a5a8d090b27435
carolinafrederico <iam@carolinafrederico.com>
2025-04-14 16:24:22 -0400

    New home content

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.engine('ejs', engine); // Enable ejs-mate for layouts
   app.set('view engine', 'ejs');
   
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(express.static('public'));
   app.use(flash());
