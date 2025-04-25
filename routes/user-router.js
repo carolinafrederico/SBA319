9409453e90a0b79dc2688447918e147de1662a00
carolinafrederico <iam@carolinafrederico.com>
2025-04-25 01:24:00 -0400

    Changes

--- routes/user-router.js
+++ routes/user-router.js
@@ -2,7 +2,6 @@ import express from 'express';
 import * as usersController from '../controllers/user-controller.js'
 const router = express.Router();
 
-
 ////////////////////////////////////////////////////
 //All User routes (INDUCES) + corresponding (CRUD)//
 ////////////////////////////////////////////////////
