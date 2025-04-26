f45605090b8d3a05c1a0f1566ffa2721b8490d5c
carolinafrederico <iam@carolinafrederico.com>
2025-04-25 20:52:57 -0400

    layout changes

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  Uid: { type: Number, unique: true, required: true },
+  Uid: { type: Number, unique: true, required: false },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  dob: { type: Date, required: true },
+  dob: { type: Date, required: false },
   joined: { type: Date, default: Date.now },
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: false },
   comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', required: false }
